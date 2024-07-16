eed8b461fdbbb06a93a0298bb98b4053d43dbb65
Sergey Kislyakov <s.kislyakov84@gmail.com>
2024-07-16 22:37:17 +0300

    add about.html

--- my_portfolio/urls.py
+++ my_portfolio/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from portfolio.views import index
+from portfolio.views import index, about
 
 urlpatterns = [
-    path('', index, name='index')
+    path('', index, name='index'),
+    path('about/', about, name='about'),
 ]
