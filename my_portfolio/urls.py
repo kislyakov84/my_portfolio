7b15e665e0628245fa48b120d97533f16a424048
Sergey Kislyakov <s.kislyakov84@gmail.com>
2024-07-18 11:43:38 +0300

    add contact form

--- my_portfolio/urls.py
+++ my_portfolio/urls.py
@@ -1,9 +1,11 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path
-from portfolio.views import index, about
+from portfolio.views import index, about, contact
+
 
 urlpatterns = [
     path('', index, name='index'),
     path('about/', about, name='about'),
+    path('contact/', contact, name='contact')
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
