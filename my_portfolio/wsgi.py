8396cf15455a05cc64bc72a60435f54b40347cf2
Sergey Kislyakov <s.kislyakov84@gmail.com>
2024-07-24 20:33:58 +0300

    wsgi for deploy

--- my_portfolio/wsgi.py
+++ my_portfolio/wsgi.py
@@ -1,10 +1,12 @@
 import os
 import sys
+sys.path.append('/opt/build')
 sys.path.append('/opt/build/my_portfolio')
 sys.path.append('/opt/build/my_portfolio/my_portfolio')
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'my_portfolio.my_portfolio.settings')
+#os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'my_portfolio.my_portfolio.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings.production')
 
 application = get_wsgi_application()
