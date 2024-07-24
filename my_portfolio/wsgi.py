55fea8f1bb4d847817eee117eeedca9a70ab3f42
Sergey Kislyakov <s.kislyakov84@gmail.com>
2024-07-24 20:55:51 +0300

    wsgi for deply2

--- my_portfolio/wsgi.py
+++ my_portfolio/wsgi.py
@@ -7,6 +7,6 @@ sys.path.append('/opt/build/my_portfolio/my_portfolio')
 from django.core.wsgi import get_wsgi_application
 
 #os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'my_portfolio.my_portfolio.settings')
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings.production')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'my_portfolio.settings')
 
 application = get_wsgi_application()
