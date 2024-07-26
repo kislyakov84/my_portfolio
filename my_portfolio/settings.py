1f00f0e7231e534e68268f8545276e8060872a68
Sergey Kislyakov <s.kislyakov84@gmail.com>
2024-07-26 22:29:00 +0300

    static_root on settings.py

--- my_portfolio/settings.py
+++ my_portfolio/settings.py
@@ -106,6 +106,7 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / "static"]
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / "media"
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
